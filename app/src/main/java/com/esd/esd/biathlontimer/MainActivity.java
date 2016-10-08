2cfe1a5ad4a0b4159bbd07c24f38a6b0e3460eac
Ivan <IvanTsel@yandex.com>
2016-10-08 22:51:29 +0300

    все для тебя

--- app/src/main/java/com/esd/esd/biathlontimer/MainActivity.java
+++ app/src/main/java/com/esd/esd/biathlontimer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.esd.esd.biathlontimer;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -20,48 +21,52 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDatabase = new DatabaseProvider(this);
-        _editText = (EditText) findViewById(R.id.editText);
-        _textView = (TextView) findViewById(R.id.textView);
+        //myDatabase = new DatabaseProvider(this);
+       // _editText = (EditText) findViewById(R.id.editText);
+       // _textView = (TextView) findViewById(R.id.textView);
     }
 
-    public void SaveDataOnClick(View view)
+    public void addFileBtn_OnClick(View view)
     {
-        SQLiteDatabase db = myDatabase.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(DatabaseProvider.DbSettings.COLUMN_SETTING_ID, 1);
-        values.put(DatabaseProvider.DbSettings.COLUMN_SETTING_NAME, "Значение в editText");
-        values.put(DatabaseProvider.DbSettings.COLUMN_SETTING_DATA, _editText.getText().toString());
-        long newRowId = db.insert(DatabaseProvider.DbSettings.TABLE_NAME, null,values);
-        _editText.setText(Long.toString(newRowId));
+        
     }
-
-    int _counter = 0;
-    int _counter2 = 0;
-    public void GetDataonClick(View view)
-    {
-        SQLiteDatabase db = myDatabase.getReadableDatabase();
-        String[] projection =
-                {
-                        DatabaseProvider.DbSettings._ID,
-                        DatabaseProvider.DbSettings.COLUMN_SETTING_ID,
-                        DatabaseProvider.DbSettings.COLUMN_SETTING_NAME,
-                        DatabaseProvider.DbSettings.COLUMN_SETTING_DATA
-                };
-        String order = DatabaseProvider.DbSettings._ID + " DESC";
-        Cursor c = db.query(DatabaseProvider.DbSettings.TABLE_NAME, projection, null, null, null, null, order);
-        c.moveToPosition(_counter2);
-        _counter++;
-        try {
-            _textView.setText(c.getString(_counter));
-        }catch (Exception e)
-        {
-            Toast.makeText(this, "Проблема"+ Integer.toString(_counter), Toast.LENGTH_SHORT).show();
-        }
-        if(_counter > 5)
-        {
-            _counter2++;
-            _counter = 0;
-        }
-    }
+//    public void SaveDataOnClick(View view)
+//    {
+//        SQLiteDatabase db = myDatabase.getWritableDatabase();
+//        ContentValues values = new ContentValues();
+//        values.put(DatabaseProvider.DbSettings.COLUMN_SETTING_ID, 1);
+//        values.put(DatabaseProvider.DbSettings.COLUMN_SETTING_NAME, "Значение в editText");
+//        values.put(DatabaseProvider.DbSettings.COLUMN_SETTING_DATA, _editText.getText().toString());
+//        long newRowId = db.insert(DatabaseProvider.DbSettings.TABLE_NAME, null,values);
+//        _editText.setText(Long.toString(newRowId));
+//    }
+//
+//    int _counter = 0;
+//    int _counter2 = 0;
+//    public void GetDataonClick(View view)
+//    {
+//        SQLiteDatabase db = myDatabase.getReadableDatabase();
+//        String[] projection =
+//                {
+//                        DatabaseProvider.DbSettings._ID,
+//                        DatabaseProvider.DbSettings.COLUMN_SETTING_ID,
+//                        DatabaseProvider.DbSettings.COLUMN_SETTING_NAME,
+//                        DatabaseProvider.DbSettings.COLUMN_SETTING_DATA
+//                };
+//        String order = DatabaseProvider.DbSettings._ID + " DESC";
+//        Cursor c = db.query(DatabaseProvider.DbSettings.TABLE_NAME, projection, null, null, null, null, order);
+//        c.moveToPosition(_counter2);
+//        _counter++;
+//        try {
+//            _textView.setText(c.getString(_counter));
+//        }catch (Exception e)
+//        {
+//            Toast.makeText(this, "Проблема"+ Integer.toString(_counter), Toast.LENGTH_SHORT).show();
+//        }
+//        if(_counter > 5)
+//        {
+//            _counter2++;
+//            _counter = 0;
+//        }
+//    }
 }
