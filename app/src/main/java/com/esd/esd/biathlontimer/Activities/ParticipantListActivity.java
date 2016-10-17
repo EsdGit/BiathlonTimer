50793a324cc41718407a5fda36a802867941720e
EsdGit <esdhelp1@gmail.com>
2016-10-17 10:48:26 +0300

    Доьавил активность

---
+++ app/src/main/java/com/esd/esd/biathlontimer/Activities/ParticipantListActivity.java
@@ -0,0 +1,20 @@
+package com.esd.esd.biathlontimer.Activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esd.esd.biathlontimer.R;
+
+/**
+ * Created by NIL_RIMS_4 on 17.10.2016.
+ */
+
+public class ParticipantListActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_participant_list);
+    }
+}
