c28d32df86def5db458b9ac9a3b9f03f97898a9f
Ivan <IvanTsel@yandex.com>
2016-12-10 21:54:26 +0300

    Проверь кнопку, и метод

--- app/src/main/java/com/esd/esd/biathlontimer/Activities/FinalActivity.java
+++ app/src/main/java/com/esd/esd/biathlontimer/Activities/FinalActivity.java
@@ -1,8 +1,10 @@
 package com.esd.esd.biathlontimer.Activities;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.Menu;
+import android.view.WindowManager;
 
 import com.esd.esd.biathlontimer.R;
 
@@ -13,5 +15,17 @@ public class FinalActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().setTitle((Html.fromHtml("<font color=\"#FFFFFF\">"  + "<big>" + getResources().getString(R.string.title_final_activity) + "</big>" + "</font>")));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.final_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
+
