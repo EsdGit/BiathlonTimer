fdd2d5dbceac4ef4851b2144fbe6726b27e89e59
Esd <esdhelp1@gmail.com>
2016-12-15 19:38:20 +0300

    Разобрался с кликом

--- app/src/main/java/com/esd/esd/biathlontimer/Activities/TestBD.java
+++ app/src/main/java/com/esd/esd/biathlontimer/Activities/TestBD.java
@@ -10,12 +10,17 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 import com.esd.esd.biathlontimer.DatabaseClasses.DatabaseProvider;
 import com.esd.esd.biathlontimer.DatabaseClasses.ParticipantSaver;
+import com.esd.esd.biathlontimer.MySimpleCursorAdapter;
 import com.esd.esd.biathlontimer.Participant;
 import com.esd.esd.biathlontimer.R;
 
@@ -25,10 +30,13 @@ import com.esd.esd.biathlontimer.R;
 
 public class TestBD extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>
 {
-    SimpleCursorAdapter scAdapter;
+    //SimpleCursorAdapter scAdapter;
+    MySimpleCursorAdapter scAdapter;
     GridView gv;
     ParticipantSaver ps;
     String name;
+    View row;
+    TableRow _row;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +48,12 @@ public class TestBD extends AppCompatActivity implements LoaderManager.LoaderCal
                 DatabaseProvider.DbParticipant.COLUMN_GROUP, DatabaseProvider.DbParticipant.COLUMN_COUNTRY};
         int[] to = new int[]{R.id.number, R.id.fio, R.id.year, R.id.group, R.id.country};
 
-        scAdapter = new SimpleCursorAdapter(this, R.layout.test_row, null, from, to, 0);
+        //scAdapter = new SimpleCursorAdapter(this, R.layout.test_row, null, from, to, 0);
+        scAdapter = new MySimpleCursorAdapter(this, R.layout.test_row, null, from, to, 0);
         gv = (GridView) findViewById(R.id.gv);
         gv.setAdapter(scAdapter);
         getSupportLoaderManager().initLoader(0, null, this);
+
     }
 
     @Override
@@ -75,7 +85,7 @@ public class TestBD extends AppCompatActivity implements LoaderManager.LoaderCal
         @Override
         public Cursor loadInBackground()
         {
-            Cursor cursor = db.getAllData(name, DatabaseProvider.DbParticipant.COLUMN_NAME+" DESC");
+            Cursor cursor = db.getAllData(name, DatabaseProvider.DbParticipant.COLUMN_NAME+" ASC");
             return cursor;
         }
     }
---
+++ app/src/main/java/com/esd/esd/biathlontimer/MySimpleCursorAdapter.java
@@ -0,0 +1,41 @@
+package com.esd.esd.biathlontimer;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by NIL_RIMS_2 on 15.12.2016.
+ */
+
+public class MySimpleCursorAdapter extends SimpleCursorAdapter
+{
+    Context localContext;
+    public MySimpleCursorAdapter(Context context, int res, Cursor res2, String[] from, int[] to, int no)
+    {
+        super(context, res, res2, from, to, no);
+        localContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View row = convertView;
+        if(row == null)
+        {
+            LayoutInflater layoutInflater = (LayoutInflater) localContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = layoutInflater.inflate(R.layout.test_row, parent, false);
+        }
+        row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(localContext, "А у меня работает", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return super.getView(position, convertView, parent);
+    }
+}
